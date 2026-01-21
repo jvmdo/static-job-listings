31696f11184101aab28afe28a18dc608ff1ffd7a
João Oliveira <deoliveira.jvm@gmail.com>
2026-01-21 03:06:08 -0300

    🎉 Scaffolds prepared

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ backgroundImage: "url(/images/bg-header-mobile.svg)" }}>
+      <img src="/icons/remove.svg" alt="x" style={{ background: "brown" }} />
+      <h1>Hello, Tanstack Query for Query parameters and Pagination</h1>
+    </div>
+  );
+}
+
+export default App;
