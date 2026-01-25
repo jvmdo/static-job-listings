1a9b11bcc08bb2c9038d6d48dd3b8f71a0af4423
João Oliveira <deoliveira.jvm@gmail.com>
2026-01-25 01:50:02 -0300

    ✨ Change filtering mode to exclusive (intersection)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,29 +1,22 @@
 import type { Job, JobFilters } from "@/use-jobs";
-import { http, HttpResponse } from "msw";
+
+import { delay, http, HttpResponse } from "msw";
 import data from "./data.json";
 
 export const handlers = [
   // TODO: does relative work for any location.href?
-  http.get("/jobs", ({ request }) => {
+  http.get("/jobs", async ({ request }) => {
+    await delay();
+
     const url = new URL(request.url);
+    const filters = {
+      role: url.searchParams.get("role"),
+      level: url.searchParams.get("level"),
+      languages: url.searchParams.getAll("languages"),
+      tools: url.searchParams.getAll("tools"),
+    };
 
-    // Given a request url of "/product?id=1",
-    // the `productId` will be a "1" string.
-    const role = url.searchParams.get("role");
-    const level = url.searchParams.get("level");
-
-    // Given a request url of "/products?id=1&id=2&id=3",
-    // the `productIds` will be an array of ["1", "2", "3"].
-    const languages = url.searchParams.getAll("languages");
-    const tools = url.searchParams.getAll("tools");
-
-    const jobs = data as Job[];
-    const filteredJobs = getFilteredJobs(jobs, {
-      role,
-      level,
-      languages,
-      tools,
-    });
+    const filteredJobs = getFilteredJobs(data, filters);
 
     return HttpResponse.json(filteredJobs);
   }),
@@ -33,18 +26,21 @@ const getFilteredJobs = (jobs: Job[], filters: JobFilters) => {
   const { role, level, languages, tools } = filters;
 
   return jobs.filter((job) => {
-    const matches = [];
+    if (role && job.role !== role) return false;
 
-    if (role) matches.push(job.role === role);
-    if (level) matches.push(job.level === level);
-    if (languages?.length) {
-      matches.push(job.languages.some((l) => languages.includes(l)));
-    }
-    if (tools?.length) {
-      matches.push(job.tools.some((t) => tools.includes(t)));
+    if (level && job.level !== level) return false;
+
+    if (
+      languages?.length &&
+      !languages.every((l) => job.languages.includes(l))
+    ) {
+      return false;
     }
 
-    // No filters = show all; otherwise show if ANY match
-    return matches.length === 0 || matches.includes(true);
+    if (tools?.length && !tools.every((t) => job.tools.includes(t))) {
+      return false;
+    }
+
+    return true;
   });
 };
