547591f1d9187898519801b2d521a2c13b91c880
João Oliveira <deoliveira.jvm@gmail.com>
2026-02-01 01:42:41 -0300

    🚑 ErrorFallback refetchQueries keys

--- src/components/error-fallback.tsx
+++ src/components/error-fallback.tsx
@@ -1,8 +1,12 @@
 import AppContainer from "@/components/app-container";
+import { useFilters } from "@/hooks/use-filters";
+import { usePage } from "@/hooks/use-page";
 import { useQueryClient } from "@tanstack/react-query";
 
 function ErrorFallback() {
   const queryClient = useQueryClient();
+  const [page] = usePage();
+  const { filters } = useFilters();
 
   return (
     <AppContainer className="mt-[30svmin] text-center items-center">
@@ -14,7 +18,9 @@ function ErrorFallback() {
         type="button"
         className="py-2 w-full rounded-sm bg-primary text-surface"
         onClick={async () => {
-          await queryClient.refetchQueries({ queryKey: ["jobs"] });
+          await queryClient.refetchQueries({
+            queryKey: ["jobs", filters, page],
+          });
         }}
       >
         Try again
